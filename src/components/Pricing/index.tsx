32055a9565f0bc9e2882e9bf3c64ba6744d93bac
ZhaoYongchao <16632301768@163.com>
2024-04-19 08:39:01 +0800

    style: price
    Signed-off-by: ZhaoYongchao <16632301768@163.com>

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -20,45 +20,35 @@ const Pricing = () => {
           center
           width="665px"
         />
-
         <div className="w-full">
-          <div className="mb-8 flex justify-center md:mb-12 lg:mb-16">
-            <span
-              onClick={() => setIsMonthly(true)}
-              className={`${isMonthly
-                ? 'pointer-events-none text-primary'
-                : 'text-dark dark:text-white'
-                } mr-4 cursor-pointer text-base font-semibold`}
-            >
-              Monthly
-            </span>
-            <div
-              onClick={() => setIsMonthly(!isMonthly)}
-              className="flex cursor-pointer items-center"
-            >
-              <div className="relative">
-                <div className="h-5 w-14 rounded-full bg-[#1D2144] shadow-inner"></div>
-                <div
-                  className={`${isMonthly ? '' : 'translate-x-full'
-                    } shadow-switch-1 absolute left-0 top-[-4px] flex h-7 w-7 items-center justify-center rounded-full bg-primary transition`}
-                >
-                  <span className="active h-4 w-4 rounded-full bg-white"></span>
-                </div>
+          <div className="mb-8 flex justify-center items-end md:mb-12 lg:mb-16">
+            <div className='flex'>
+              <div
+                onClick={() => setIsMonthly(false)}
+                className={`${isMonthly
+                  ? 'text-dark dark:text-[#747d94] border dark:border-slate-700 pr-6 -mr-4 '
+                  : 'text-primary dark:text-white pointer-events-non border dark:border-slate-400 border-blue-700 bg-blue-50 z-10 dark:bg-gray-dark'
+                  } p-2 cursor-pointer text-sm font-semibold text-center rounded-md `}
+              >
+                Years
+                <svg className='inline-block mx-2' width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z" />
+                  <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z" />
+                </svg>
+                Get ~2 months free
+              </div>
+              <div
+                onClick={() => setIsMonthly(true)}
+                className={`${isMonthly
+                  ? 'pointer-events-none text-primary border dark:border-slate-400 border-blue-700 bg-blue-50 dark:text-white dark:bg-gray-dark'
+                  : 'text-dark pl-6 -ml-4 dark:border-slate-700 dark:text-[#747d94]'
+                  } p-2 cursor-pointer text-sm font-semibold rounded-md border`}
+              >
+                Monthly
               </div>
             </div>
-            <span
-              onClick={() => setIsMonthly(false)}
-              className={`${isMonthly
-                ? 'text-dark dark:text-white'
-                : 'pointer-events-none text-primary'
-                } ml-4 cursor-pointer text-base font-semibold`}
-            >
-              Yearly
-              <span className=' ml-2 text-sm'>Pay 10 get 12</span>
-            </span>
           </div>
         </div>
-
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
           <PricingBox
             packageName="Lite"
