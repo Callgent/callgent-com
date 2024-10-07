7e119c372fa035e0fff7b265953c97dc42d1e1d3
dev-callgent <dev@callgent.com>
2024-10-07 15:53:55 +0800

    feat: newletter

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
-              className="mb-12 relative rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              className="relative rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
               "
             >
