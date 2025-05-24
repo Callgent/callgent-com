7866712d7c083c087c9838a91d29d06c55bce1be
dev-callgent <dev@callgent.com>
2025-05-24 14:14:01 +0800

    ci: steprun.ai
    Signed-off-by: dev-callgent <dev@callgent.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -154,7 +154,7 @@ const Footer = () => {
                     href={'https://steprun.ai'}
                     className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                   >
-                    Steprun.ai
+                    𑢡teprun.ai
                   </Link>
                 </li>
               </ul>
